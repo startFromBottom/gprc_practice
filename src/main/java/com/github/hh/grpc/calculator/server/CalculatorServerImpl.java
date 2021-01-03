99e1a282da6079c86f34991df83f89eed63aa9c7
uhh0701@gmail.com <uhh0701@gmail.com>
2021-01-04 00:32:07 +0900

    [Client Streaming] implement server

--- src/main/java/com/github/hh/grpc/calculator/server/CalculatorServerImpl.java
+++ src/main/java/com/github/hh/grpc/calculator/server/CalculatorServerImpl.java
@@ -35,4 +35,5 @@ public class CalculatorServerImpl extends CalculatorServiceGrpc.CalculatorServic
         responseObserver.onCompleted();
 
     }
+
 }
--- src/main/java/com/github/hh/grpc/greeting/server/GreetServiceImpl.java
+++ src/main/java/com/github/hh/grpc/greeting/server/GreetServiceImpl.java
@@ -28,6 +28,8 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
 
     @Override
     public void greetManyTimes(GreetManyTimesRequest request, StreamObserver<GreetManyTimesResponse> responseObserver) {
+        // server streaming
+
         String firstName = request.getGreeting().getFirstName();
 
         try {
@@ -44,5 +46,39 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
         } finally {
             responseObserver.onCompleted();
         }
+    }
+
+    @Override
+    public StreamObserver<LongGreetRequest> longGreet(StreamObserver<LongGreetResponse> responseObserver) {
+        // client streaming
+        StreamObserver<LongGreetRequest> requestObserver = new StreamObserver<LongGreetRequest>() {
+
+            String result = "";
+
+            @Override
+            public void onNext(LongGreetRequest value) {
+                // client sends a message
+                result += ". Hello " + value.getGreeting().getFirstName() + "! ";
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // client sends an error
+            }
+
+            @Override
+            public void onCompleted() {
+                // client is done
+                responseObserver.onNext(
+                        LongGreetResponse.newBuilder()
+                                .setResult(result)
+                                .build()
+                );
+                responseObserver.onCompleted();
+
+            }
+        };
+
+        return super.longGreet(responseObserver);
     }
 }
