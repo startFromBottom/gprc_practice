85da9e257ebd34256f4bcf559b9795ebdb9c0155
uhh0701@gmail.com <uhh0701@gmail.com>
2021-01-04 00:38:20 +0900

    [Client Streaming] refactor GreetingClient.java

--- src/main/java/com/github/hh/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/hh/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,5 @@
 package com.github.hh.grpc.greeting.client;
 
-import com.proto.dummy.DummyServiceGrpc;
 import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -10,55 +9,64 @@ public class GreetingClient {
     public static void main(String[] args) {
         System.out.println("Hello I'm a gRPC client");
 
+        GreetingClient main = new GreetingClient();
+        main.run();
+
+        System.out.println("Creating Stub");
+
+    }
+
+    public void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext() // not use in production
                 .build();
 
+        doUnaryCall(channel);
+        doServerStreamingCall(channel);
 
-        System.out.println("Creating Stub");
-        // old and dummy
-//        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
-        // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+        // do something
+        System.out.println("Shutting down channel");
+        channel.shutdown();
 
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
 
         // created a greet service client (blocking - synchronous)
         GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
 
-        // 1. Unary
-//
-//        // created a protocol buffer greeting message
-//        Greeting greeting = Greeting.newBuilder()
-//                .setFirstName("Stitch")
-//                .setLastName("HH")
-//                .build();
-//
-//        // do the same for a GreetRequest
-//        GreetRequest greetRequest = GreetRequest.newBuilder()
-//                .setGreeting(greeting)
-//                .build();
-//
-//        // call the RPC and get back a GreetResponse(protocol buffers)
-//        // 마치 server의 greet 메서드를 직접 호출하는 것처럼 보이지만, 네트워크를 타는 것!
-//        GreetResponse greetResponse = greetClient.greet(greetRequest);
-//
-//        System.out.println(greetResponse.getResult());
+        // created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Stitch")
+                .setLastName("HH")
+                .build();
 
-        // 2. Server Streaming
+        // do the same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a GreetResponse(protocol buffers)
+        // 마치 server의 greet 메서드를 직접 호출하는 것처럼 보이지만, 네트워크를 타는 것!
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+    }
+
+    private void doServerStreamingCall(ManagedChannel channel) {
+
+        // created a greet service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
         GreetManyTimesRequest greetManyTimesRequest =
                 GreetManyTimesRequest.newBuilder()
-                .setGreeting(Greeting.newBuilder().setFirstName("hh"))
-                .build();
+                        .setGreeting(Greeting.newBuilder().setFirstName("hh"))
+                        .build();
 
         // we stream the responses (in blocking manner)
         greetClient.greetManyTimes(greetManyTimesRequest)
                 .forEachRemaining(greetManyTimesResponse -> {
                     System.out.println(greetManyTimesResponse.getResult());
                 });
-
-        // do something
-        System.out.println("Shutting down channel");
-        channel.shutdown();
-
     }
 
 }
