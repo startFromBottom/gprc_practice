11cbc2a508f06e0f97281e8ee5a4d1f55157e837
uhh0701@gmail.com <uhh0701@gmail.com>
2021-01-03 21:21:55 +0900

    [Unary] implement server

---
+++ src/main/java/com/github/hh/grpc/greeting/server/GreetServiceImpl.java
@@ -0,0 +1,28 @@
+package com.github.hh.grpc.greeting.server;
+
+import com.proto.greet.*;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+
+        // extract the fields we need
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+        String result = "Hello " + firstName;
+
+        // create the response
+        GreetResponse response = GreetResponse.newBuilder()
+                .setResult(result)
+                .build();
+
+        // send the response
+        responseObserver.onNext(response);
+
+        // complete the RPC call
+        responseObserver.onCompleted();
+
+    }
+}
