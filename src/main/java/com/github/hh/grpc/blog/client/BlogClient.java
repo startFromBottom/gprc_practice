cb628bc0d177101503eabc184fbd759cbb3b0a8c
uhh0701@gmail.com <uhh0701@gmail.com>
2021-01-05 21:36:13 +0900

    implement CreateBlog Client

--- src/main/java/com/github/hh/grpc/blog/client/BlogClient.java
+++ src/main/java/com/github/hh/grpc/blog/client/BlogClient.java
@@ -1,6 +1,13 @@
 package com.github.hh.grpc.blog.client;
 
 
+import com.proto.calculator.Blog;
+import com.proto.calculator.BlogServiceGrpc;
+import com.proto.calculator.CreateBlogRequest;
+import com.proto.calculator.CreateBlogResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class BlogClient {
 
     public static void main(String[] args) {
@@ -10,6 +17,26 @@ public class BlogClient {
     }
 
     private void run() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        BlogServiceGrpc.BlogServiceBlockingStub blogClient = BlogServiceGrpc.newBlockingStub(channel);
+        Blog blog = Blog.newBuilder()
+                .setAuthorId("aaa")
+                .setTitle("New blog!")
+                .setContent("Hello world this is my first blog!")
+                .build();
+
+        CreateBlogResponse createResponse = blogClient.createBlog(
+                CreateBlogRequest.newBuilder()
+                        .setBlog(blog)
+                        .build()
+        );
+
+        System.out.println("Received create blog response");
+        System.out.println(createResponse.toString());
+
     }
 }
 
